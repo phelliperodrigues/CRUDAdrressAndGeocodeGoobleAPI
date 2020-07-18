80564421c7ee6eea0e05830c0374f452745c0f12
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2020-07-18 13:56:11 -0300

    Add validation from No Data Found in resource get findById

--- src/main/java/com/stoom/address/controller/AddressController.java
+++ src/main/java/com/stoom/address/controller/AddressController.java
@@ -5,10 +5,12 @@ import com.stoom.address.repository.AddressRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/address")
@@ -20,5 +22,12 @@ public class AddressController {
     @GetMapping
     public ResponseEntity<List<Address>> findAll(){
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Address> getAddressById(@PathVariable("id") String uuid) {
+        return repository.findById(UUID.fromString(uuid))
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
--- src/test/java/com/stoom/address/controller/AddressControllerTest.java
+++ src/test/java/com/stoom/address/controller/AddressControllerTest.java
@@ -14,8 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -117,19 +115,12 @@ public class AddressControllerTest {
     @DisplayName("[R] Testing return from find by Id Invalid")
     public void should_return_a_address_from_find_by_id_invalid() throws Throwable {
 
-        String responseBody = this.mvc.perform(
+        this.mvc.perform(
                 get(uri +  UUID.randomUUID().toString())
                         .contentType(APPLICATION_JSON_UTF8)
                         .accept(APPLICATION_JSON_UTF8)
         )
-                .andExpect(status().isOk())
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
-
-        Address addressReturn = objectMapper.readValue(responseBody, Address.class);
-
-        assertThat(addressReturn).extracting(Address::getId).isNull();
+                .andExpect(status().isNotFound());
     }
 
 }
