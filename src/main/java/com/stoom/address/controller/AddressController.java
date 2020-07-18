90bd527ee83a23b1dc275f8bea146bb8714af203
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2020-07-18 11:26:36 -0300

    Implements methods from return all Addrress in get resource

--- src/main/java/com/stoom/address/controller/AddressController.java
+++ src/main/java/com/stoom/address/controller/AddressController.java
@@ -1,6 +1,8 @@
 package com.stoom.address.controller;
 
 import com.stoom.address.model.Address;
+import com.stoom.address.repository.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +14,11 @@ import java.util.List;
 @RequestMapping("/address")
 public class AddressController {
 
+    @Autowired
+    private AddressRepository repository;
+
     @GetMapping
     public ResponseEntity<List<Address>> findAll(){
-        return null;
+        return ResponseEntity.ok(repository.findAll());
     }
 }
